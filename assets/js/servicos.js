1efdf6fe60c50a3b2eeaaef48cb8ecd84aead162
Andre Paiva <andrepaiva@live.com>
2021-12-10 22:40:29 -0300

    commit atualizacao 2

---
+++ assets/js/servicos.js
@@ -0,0 +1,9 @@
+window.addEventListener("scroll", function() {showFunction()});
+
+function showFunction() {
+    if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+        document.getElementById("link-topo").style.display = "block";
+    } else {
+        document.getElementById("link-topo").style.display = "none";
+    }
+}
